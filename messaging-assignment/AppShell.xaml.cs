ca71abcad413194d049a37660d12e4dcb448827f
Szatmári Boldizsár <szatmariboldizsar@gmail.com>
2026-03-26 19:00:40 +0100

    Add project files.

---
+++ messaging-assignment/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace messaging_assignment
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ messaging-assignment/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace messaging_assignment
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
